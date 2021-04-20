7e552cccf5e11a9dd42b830979ef997a6401742c
d0hyeon <ehgus6887@gmail.com>
2021-04-20 15:32:26 +0900

    feat: user atom state 추가

---
+++ src/atoms/user.ts
@@ -0,0 +1,20 @@
+import { User } from './../types/index';
+import { atom, selector } from 'recoil';
+
+const STORAGE_KEY = 'CHAT_APP';
+const STORAGE_USER_KEY = 'USER';
+const localStorageUser = localStorage.getItem(`${STORAGE_KEY}/${STORAGE_USER_KEY}`);
+
+export const userState = atom<User | null>({
+  key: 'userState',
+  default:  localStorageUser ? JSON.parse(localStorageUser) : null,
+});
+
+export const userSelector = selector<User | null>({
+  key: 'userSelector',
+  get: ({get}) => get(userState),
+  set: ({set}, state) => {
+    localStorage.setItem(`${STORAGE_KEY}/${STORAGE_USER_KEY}`, JSON.stringify(state));
+    set(userState, state as User);
+  }
+}) 
