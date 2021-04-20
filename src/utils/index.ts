c12bb4254ce7d26b39b1bc346cae520ab758d74c
d0hyeon <ehgus6887@gmail.com>
2021-04-20 15:31:01 +0900

    feat: 유틸함수 추가

---
+++ src/utils/index.ts
@@ -0,0 +1,40 @@
+import { User } from './../types/index';
+export const isExistWithInTimeout = <T = unknown>(value: T, timeout: number = 3000, repeatUnit: number = 300): Promise<T | null> => {
+  let durationTime = 0;
+
+  return new Promise((resolve, reject) => {
+    const recursiveCheck = (startTime: Date) => {
+      if(value) {
+        if(value instanceof Object) {
+          if(Object.values(value).every(value => !!value)) {
+            return resolve(value);
+          }
+        } else if(value instanceof Array) {
+          if(value.every(value => !!value)) {
+            return resolve(value);
+          }
+        } else {
+          return resolve(value as T);
+        }
+      }
+      const date = new Date();
+      const currentTime = date.getTime() - startTime.getTime();
+      durationTime += currentTime;
+      
+      if(durationTime > timeout) {
+        return reject(null);
+      } 
+
+      setTimeout(() => recursiveCheck(date), repeatUnit);
+    }
+    recursiveCheck(new Date());
+  })
+}
+
+export const getUser = (): User | null => {
+  const userStr = localStorage.getItem('user');
+  if(userStr) {
+    return JSON.parse(userStr);
+  }
+  return null;
+}
