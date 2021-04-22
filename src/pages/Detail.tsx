abc7789d4b9cf7ee1857b346251d642397e0b563
d0hyeon <ehgus6887@gmail.com>
2021-04-22 14:43:10 +0900

    feat: detail 컴포넌트 > 컴포넌트 세분화

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,77 +1,37 @@
 import React from 'react'
-import { io } from 'socket.io-client';
 import { useParams } from 'react-router-dom';
 import { Room } from '@src/types';
-import Loading from '@src/components/common/Loading';
-import PasswordPopup from '@src/components/popup/PasswordPopup';
 import ChatRoom from './Room';
-import { useRecoilValue } from 'recoil';
-import { userSelector } from '@src/atoms/user';
-const socket = io();
-
-enum PasswordState {
-  UNKNOWN = 'unknown',
-  PENDING = 'pending',
-  SUCCESS = 'success'
-}
+import RoomGate from './RoomGate';
 
 const Detail = () => {
-  const user = useRecoilValue(userSelector);
   const {room} = useParams<{room: string}>();
-  const [roomInfo, setRoomInfo] = React.useState<Room>();
-  const [passwordState, setPasswordState] = React.useState<PasswordState>(PasswordState.UNKNOWN);
+  const [roomData, setRoomData] = React.useState<Room>();
   const [tracks, setTracks] = React.useState<MediaStreamTrack[]>([]);
-
-  const isContainInRoom = React.useMemo(() => roomInfo?.users.some(({id}) => id === user!.id) ?? true, [roomInfo]);
   
-  const successPwHandler = React.useCallback(() => {
-    setPasswordState(PasswordState.SUCCESS);
-  }, [setPasswordState]);
+  const onSuccessGate = React.useCallback((room: Room) => setRoomData(room), [setRoomData]);
 
   React.useEffect(() => {
     navigator.mediaDevices.getUserMedia({video: true, audio: true})
       .then((mediaStream) => {
         setTracks(mediaStream.getTracks());
       });
-  }, [setTracks]);
-
-  React.useEffect(() => {
-    const roomDetailHandler = (room: Room) => {
-      setRoomInfo(room);
-    }
-    socket.emit('getRoomDetail', room);
-    socket.on('roomDetail', roomDetailHandler);
 
     return () => {
-      socket.off('roomDetail', roomDetailHandler);
+      setTracks(tracks => {
+        tracks.forEach(track => track.stop());
+        return [];
+      })
     }
-  }, [room, setRoomInfo]);
-
-  React.useEffect(() => {
-    if(roomInfo && roomInfo.isPassword) {
-      if(!isContainInRoom) {
-        setPasswordState(PasswordState.PENDING);
-        return;
-      }
-    }
-    setPasswordState(PasswordState.SUCCESS);
-  }, [roomInfo, isContainInRoom, setPasswordState]);
-
-
+  }, [setTracks]);
+  
   return (
-    passwordState === PasswordState.SUCCESS && 
-    tracks.length && 
-    roomInfo
-  ) ? (
-    <ChatRoom roomId={room} roomInfo={roomInfo} tracks={tracks} />
-  ) : (
-    <>
-      {roomInfo?.isPassword && (
-        <PasswordPopup roomId={room} open={passwordState === PasswordState.PENDING} onSuccess={successPwHandler} />
-      )}
-      <Loading />
-    </>
+    (roomData && tracks.length) 
+      ? <ChatRoom roomId={room} roomInfo={roomData} tracks={tracks} />  
+      : <RoomGate onSuccess={onSuccessGate} />
+    
+    
   )
 }
 
-export default Detail;
+export default React.memo(Detail);
