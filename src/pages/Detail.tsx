9cc466ef62c9e82b915eb03bf1cf8648bc82fb63
d0hyeon <ehgus6887@gmail.com>
2021-04-22 14:53:09 +0900

    fix: 미디어 스트림 종료하지 않던 문제 수정

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -4,26 +4,29 @@ import { Room } from '@src/types';
 import ChatRoom from './Room';
 import RoomGate from './RoomGate';
 
+type MutableRef<T> = {current: T | null};
+
 const Detail = () => {
   const {room} = useParams<{room: string}>();
   const [roomData, setRoomData] = React.useState<Room>();
   const [tracks, setTracks] = React.useState<MediaStreamTrack[]>([]);
+  const mediaStreamRef: MutableRef<MediaStream> = React.useRef(null);
   
   const onSuccessGate = React.useCallback((room: Room) => setRoomData(room), [setRoomData]);
 
   React.useEffect(() => {
     navigator.mediaDevices.getUserMedia({video: true, audio: true})
-      .then((mediaStream) => {
+      .then((mediaStream: MediaStream) => {
+        mediaStreamRef.current = mediaStream;
         setTracks(mediaStream.getTracks());
       });
 
     return () => {
-      setTracks(tracks => {
-        tracks.forEach(track => track.stop());
-        return [];
-      })
+      if(mediaStreamRef.current) {
+        mediaStreamRef.current.getTracks().forEach(track => track.stop());
+      }
     }
-  }, [setTracks]);
+  }, [setTracks, mediaStreamRef]);
   
   return (
     (roomData && tracks.length) 
