f3644b55f0b5d554839d7424d2104189dfaa28b7
d0hyeon <ehgus6887@gmail.com>
2021-05-12 18:24:13 +0900

    fix: 상세 > 검증 후 성공 이후에 유저 미디어 요청

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { Room } from '@src/types';
 import ChatRoom from './Room';
 import RoomGate from './RoomGate';
@@ -7,33 +7,40 @@ import RoomGate from './RoomGate';
 type MutableRef<T> = {current: T | null};
 
 const Detail = () => {
+  const history = useHistory();
   const {room} = useParams<{room: string}>();
   const [roomData, setRoomData] = React.useState<Room>();
   const [tracks, setTracks] = React.useState<MediaStreamTrack[]>([]);
   const mediaStreamRef: MutableRef<MediaStream> = React.useRef(null);
+  const [initStream, setInitStream] = React.useState<boolean>(false);
   
   const onSuccessGate = React.useCallback((room: Room) => setRoomData(room), [setRoomData]);
+  const onFailGate = React.useCallback((message) => {
+    alert(message);
+    history.replace('/');
+  }, [history, mediaStreamRef]);
 
   React.useEffect(() => {
-    navigator.mediaDevices.getUserMedia({video: true, audio: true})
-      .then((mediaStream: MediaStream) => {
-        mediaStreamRef.current = mediaStream;
-        setTracks(mediaStream.getTracks());
-      });
+    if(roomData) {
+      navigator.mediaDevices.getUserMedia({video: true, audio: true})
+        .then((mediaStream: MediaStream) => {
+          mediaStreamRef.current = mediaStream;
+          setTracks(mediaStream.getTracks());
+          setInitStream(true);
+        });
 
-    return () => {
-      if(mediaStreamRef.current) {
-        mediaStreamRef.current.getTracks().forEach(track => track.stop());
+      return () => {
+        if(mediaStreamRef.current) {
+          mediaStreamRef.current.getTracks().forEach(track => track.stop());
+        }
       }
     }
-  }, [setTracks, mediaStreamRef]);
+  }, [roomData, setTracks, setInitStream, mediaStreamRef]);
   
   return (
-    (roomData && tracks.length) 
+    (roomData && initStream) 
       ? <ChatRoom roomId={room} roomInfo={roomData} tracks={tracks} />  
-      : <RoomGate onSuccess={onSuccessGate} />
-    
-    
+      : <RoomGate onSuccess={onSuccessGate} onFail={onFailGate} />
   )
 }
 
